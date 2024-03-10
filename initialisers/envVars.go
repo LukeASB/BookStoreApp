3f21778fdd2f923137931661773ba252b7e7210e
Luke Sloane-Bulger <luke.sloanebulger@protonmail.com>
2024-03-10 16:32:02 +0000

    main - update

--- initialisers/envVars.go
+++ initialisers/envVars.go
@@ -7,6 +7,11 @@ import (
 	"github.com/joho/godotenv"
 )
 
+/*
+LoadEnvVariables loads environment variables from the .env file using godotenv.
+It checks if the .env file exists and loads its content. If successful, it continues execution.
+If the file cannot be loaded, it logs a fatal error and exits the application.
+*/
 func LoadEnvVariables() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading the .env file.")
@@ -16,7 +21,11 @@ func LoadEnvVariables() {
 	checkEnvVariablesArePopulated()
 }
 
-// Check if each required env var is set before continuing app execution.
+/*
+checkEnvVariablesArePopulated checks if each required environment variable is set before continuing the application execution.
+Iterates over a list of required environment variables and checks if each one is set.
+If any required variable is not set, it logs a fatal error and exits the application.
+*/
 func checkEnvVariablesArePopulated() {
 	requiredEnvVars := []string{"DB_URL", "ENV", "PORT", "SITE_URL", "VERSION"}
 
